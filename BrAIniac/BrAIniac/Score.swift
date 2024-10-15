940f29abd398cd81f5e186415cab2117bef3a757
Sam Miao <sam.miao@tum.de>
2024-10-16 01:46:01 +0200

    add external sound package

--- BrAIniac/BrAIniac/Score.swift
+++ BrAIniac/BrAIniac/Score.swift
@@ -6,15 +6,23 @@
 //
 
 import SwiftUI
+import Subsonic
 
 @Observable class Score {
     var totalCorrect: Int = 0
     var totalWrong: Int = 0
+    let soundPlayer = SubsonicController.shared
+    let soundsCorrect = 4
+    let soundsWrong = 5
     
     func updateScore(isCorrect: Bool) {
         if isCorrect {
+            let num = Int.random(in: 1...soundsCorrect)
+            soundPlayer.play(sound: "correct-\(num).mp3")
             totalCorrect += 1
         } else {
+            let num = Int.random(in: 1...soundsWrong)
+            soundPlayer.play(sound: "wrong-\(num).mp3")
             totalWrong += 1
         }
     }
